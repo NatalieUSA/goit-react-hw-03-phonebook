eccc4e616400469e2a5c3f4fab91eefe3f0b7624
NatalieUSA <110269066+NatalieUSA@users.noreply.github.com>
2023-03-03 09:23:55 +0200

    add src, function
    componentDidMount, componentDidUpdate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import { GlobalStyle } from './GlobalStyle';
+
+import { Title } from './shared/Title/Title';
+import { Phonebook } from './Phonebook/Phonebook';
+import { Layout } from './Layout';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Layout>
+      <GlobalStyle />
+      <Title>Phonebook</Title>
+
+      <Phonebook />
+    </Layout>
   );
 };
